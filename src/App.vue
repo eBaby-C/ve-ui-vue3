0e99923f194a5b91a8ab1f548dbc6769f5ac2412
eBaBy-C <udbrkve@gmail.com>
2023-02-13 21:37:32 +0800

    准备写一个五星好评

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Stars :default-num="starNum" :size="size" @getStarNum="getStarNum" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  setup() {
+    const getStarNum = (num) => {
+
+    }
+
+
+    return {
+      getStarNum
+    }
+  },
+
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/libs/jsui/index.js
@@ -0,0 +1,1 @@
+import {useStars} from './useStars';
